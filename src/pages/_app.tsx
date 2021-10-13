d9a6b9c9a979e366a9a63315131b903696fd9bd9
AbnerWill <abner@rocksoftware.com.br>
2021-10-13 16:31:43 -0300

    :heavy_plus_sign: Added react slick carousel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,20 @@
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 import '../styles/globals.scss'
 import { AppProps } from 'next/dist/shared/lib/router/router'
+import SSRProvider from 'react-bootstrap/SSRProvider'
 
 library.add(fab, faCoffee)
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <>
+    <SSRProvider>
       <Component {...pageProps} />
-    </>
+    </SSRProvider>
   )
 }
 
--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,41 +1,59 @@
+import Slider from 'react-slick'
+
+import { Footer } from '../components/Footer'
 import NavbarVoit from '../components/Navbar'
 import { ProductItemCard } from '../components/ProductItemCard'
 import { ProductsFilter } from '../components/ProductsFilter'
 import { ProdutoDestaque } from '../components/ProdutoDestaque'
+import { ProdutosProximos } from '../components/ProdutosProximos'
+import { SelecaoEsporte } from '../components/SelecaoEsporte'
 
 import Styles from '../styles/products.module.scss'
 
 export default function product(): JSX.Element {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1
+  }
+
   return (
     <>
       <NavbarVoit />
       <ProdutoDestaque />
       <main className={Styles.main}>
-        <ProductsFilter />
-        <section className={Styles.productsGrid}>
-          <ProductItemCard />
-          <ProductItemCard />
-          <ProductItemCard />
-          <ProductItemCard />
-          <ProductItemCard />
-          <ProductItemCard />
-          <ProductItemCard />
-          <ProductItemCard />
-          <ProductItemCard />
-          <ProductItemCard />
-          <ProductItemCard />
-          <ProductItemCard />
-          <ProductItemCard />
-          <ProductItemCard />
-          <ProductItemCard />
-          <ProductItemCard />
-          <ProductItemCard />
-          <ProductItemCard />
-          <ProductItemCard />
-          <ProductItemCard />
-          <ProductItemCard />
-        </section>
+        <div className={Styles.wrapper}>
+          <ProductsFilter />
+          <section className={Styles.productsGrid}>
+            <ProductItemCard />
+            <ProductItemCard />
+            <ProductItemCard />
+            <ProductItemCard />
+            <ProductItemCard />
+            <ProductItemCard />
+            <ProductItemCard />
+            <ProductItemCard />
+            <ProductItemCard />
+            <ProductItemCard />
+            <ProductItemCard />
+            <ProductItemCard />
+            <ProductItemCard />
+            <ProductItemCard />
+            <ProductItemCard />
+            <ProductItemCard />
+            <ProductItemCard />
+            <ProductItemCard />
+            <ProductItemCard />
+            <ProductItemCard />
+            <ProductItemCard />
+          </section>
+        </div>
+        <SelecaoEsporte />
+        <ProdutosProximos />
       </main>
+      <Footer />
     </>
   )
 }
