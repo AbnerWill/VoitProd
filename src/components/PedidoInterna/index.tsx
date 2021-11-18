50470a7295d4cff1bc984398203be708d08dd203
alvinho2203 <andrezo10@hotmail.com>
2021-11-18 17:24:18 -0300

    Dados Usuario

--- src/components/PedidoInterna/index.tsx
+++ src/components/PedidoInterna/index.tsx
@@ -13,6 +13,7 @@ import { Spinner } from 'react-bootstrap'
 import { mascaraCPF } from '../../utils/mascaraCPF'
 import { mascaraCelular } from '../../utils/mascaraCelular'
 import { mascaraCep } from '../../utils/mascaraCep'
+import { mascaraData } from '../../utils/mascaraData'
 import { CustomField } from '../../components/CustomField'
 import { CustomDropdown } from '../CustomDropdown'
 
@@ -68,8 +69,19 @@ export function PedidoInterna(): JSX.Element {
     email: string
     uf: string
   }
+
+  interface DadosUsuario{
+    nome_completo: string
+    data_nascimento:  string
+    cpf: string
+    telefone: string
+    email: string
+    senha: string
+  }
+  
   const regexCpf = /^(([0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}))$/
   const regexCep = 	/^\d{5}-\d{3}$/
+  const regexDataNasc = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/
 
   const schema = Yup.object().shape({
     nome_fantasia: Yup.string().required('Este campo é obrigatório'),
@@ -95,11 +107,47 @@ export function PedidoInterna(): JSX.Element {
       .required('Este campo é obrigatório')
   })
 
+  const schema2 = Yup.object().shape({
+    nome_completo: Yup.string().required('Este campo é obrigatório'),
+    data_nascimento: Yup.string()
+    .max(10)
+    .matches(regexDataNasc, 'Digite uma data válida')
+    .required('Este campo é obrigatório'),
+    cpf: Yup.string()
+    .max(14)
+    .matches(regexCpf, 'Digite um CPF do tipo XXX.XXX.XXX-XX')
+    .required('Este campo é obrigatório'),
+    telefone: Yup.string()
+    .min(13, 'Digite o numero completo')
+    .required('Este campo é obrigatório'),
+    email: Yup.string()
+    .email('Digite um email válido')
+    .required('Este campo é obrigatório'),
+    senha: Yup.string().required('Este campo é obrigatório'),
+    novasenha: Yup.string().required('Digite a nova senha')
+  })
+
   async function onSubmit(dados: DadosCadastroLoja) {
     try {
       setMensagemErro([''])
       setLoading(true)
       const response = await api.post('/loja', dados)
+      setLoading(false)
+
+    } catch (error) {
+      setLoading(false)
+      setMensagemErro([
+        ...mensagemErro
+      ])
+      console.log(mensagemErro)
+    }
+  }
+
+  async function onSubmit2(dados: DadosUsuario) {
+    try {
+      setMensagemErro([''])
+      setLoading(true)
+      const response = await api.post('/usuario', dados)
       setLoading(false)
 
     } catch (error) {
@@ -228,91 +276,94 @@ export function PedidoInterna(): JSX.Element {
 
   const MeusDados = () => {
     return (
-      <div className={`${Styles.divPedidos} col-12 col-md-10`}>
-        <div className={Styles.divInputs}>
-          <div className="row">
-            <div className={`${Styles.inputsForm} col-12 col-sm-5`}>
-              <label htmlFor="nome">Nome Completo</label>
-              <input
-                type="text"
-                id="nome"
-                placeholder="Nome Completo"
-                onBlur={evt => setNome(evt.target.value)}
-              />
-              <label htmlFor="dataNascimento">Data Nascimento</label>
-              <input
-                type="text"
-                id="dataNascimento"
-                placeholder="Data Nascimento"
-              />
-              <label htmlFor="cpf">CPF</label>
-              <input type="text" id="cpf" placeholder="CPF" />
-              <div className={Styles.divRadio}>
-                <input type="radio" id="masculino" />
-                <label htmlFor="masculino">Masculino</label>
-                <input type="radio" id="feminino" />
-                <label htmlFor="feminino">Feminino</label>
-              </div>
-              <label htmlFor="telefone">Telefone</label>
-              <input type="text" placeholder="Telefone" />
-            </div>
-            <div className={`${Styles.divSeparador} col-1`}></div>
-            <div className={`${Styles.inputsForm} col-12 col-sm-6`}>
-              <label htmlFor="acesso">Dados de Acesso</label>
-              <input type="text" id="acesso" placeholder="Dados de Acesso" />
-              <label htmlFor="senha">Senha</label>
-              <input type="password" id="senha" placeholder="Senha" />
-              <label htmlFor="novaSenha">Nova Senha</label>
-              <input type="password" id="novaSenha" placeholder="Nova Senha" />
-              <p className={Styles.checkboxTexto}>Preferências</p>
-              <div className="d-flex align-items-center ">
-                <input
-                  type="checkbox"
-                  id="checkbox1"
-                  className={Styles.checkboxPreferencias}
-                />
-                <label htmlFor="checkbox1">
-                  Quero receber por e-mail ofertas e novidades
-                </label>
-              </div>
-              <div className="d-flex align-items-center ">
-                <input
-                  type="checkbox"
-                  id="checkbox2"
-                  className={Styles.checkboxPreferencias}
-                />
-                <label htmlFor="checkbox2">
-                  Quero receber notificações dos meus pedidos por SMS
-                </label>
-              </div>
-            </div>
-          </div>
-        </div>
-        <p className={Styles.titulo}>Endereço</p>
-        <div className={`${Styles.divInputs} mt-5`}>
-          <div className="row">
-            <div className="col-12 col-sm-6 col-lg-4">
-              <div className={Styles.divEndereco}>
-                <p className={Styles.nome}>Nome da pessoa</p>
-                <div className={Styles.endereco}>
-                  <span>Rua Santos Mauro, 468</span>
-                  <span>Centro - Ponta Grossa - PR</span>
-                  <span>CEP: 84063-160</span>
-                  <div className="d-flex justify-content-end">
-                    <i className="fas fa-pen me-2">
-                      <span>Alterar</span>
-                    </i>
-                    <i className="fas fa-trash me-2 ">
-                      <span>Excluir</span>
-                    </i>
+      <Formik
+      validationSchema={schema2}
+      validateOnBlur
+      initialValues={{
+        nome_completo: '',
+        data_nascimento: '',
+        cpf: '',
+        telefone: '',
+        email: '',
+        senha:'',
+        novasenha:''
+      }}
+      onSubmit={values => onSubmit2(values)}
+      >
+            {({ values, dirty, errors }) => (
+            <div className={`${Styles.divPedidos} col-12 col-md-10`}>
+              <Form>
+
+                <div className={Styles.divInputs}>
+                  <div className="row">
+                      <div className={`${Styles.inputsForm} col-12 col-sm-5`}>
+                          <CustomField
+                            type="text"
+                            name="nome_completo"
+                            label="Nome Completo*"
+                            dirty={dirty}
+                          />
+                          <CustomField
+                            type="text"
+                            name="data_nascimento"
+                            label="Data de Nascimento*"
+                            maxLength={10}
+                            value={mascaraData(values.data_nascimento)}
+                            dirty={dirty}
+                          />
+                          <CustomField
+                            type="text"
+                            name="cpf"
+                            label="CPF*"
+                            maxLength={14}
+                            value={mascaraCPF(values.cpf)}
+                            dirty={dirty}
+                          />
+                          <CustomField
+                            type="phone"
+                            name="telefone"
+                            label="Telefone"
+                            value={mascaraCelular(values.telefone)}
+                            maxLength={14}
+                            dirty={dirty}
+                          />
+                      </div>
+                      <div className={`${Styles.divSeparador} col-1`}></div>
+                      <div className={`${Styles.inputsForm} col-12 col-sm-5`}>
+                          <CustomField
+                            type="email"
+                            name="email"
+                            label="E-mail*"
+                            dirty={dirty}
+                          />
+                          <CustomField
+                            type="text"
+                            name="senha"
+                            label="Senha*"
+                            dirty={dirty}
+                          />
+                          <CustomField
+                            type="text"
+                            name="novasenha"
+                            label="Nova Senha*"
+                            dirty={dirty}
+                          />
+                        <button type="submit" className={`${Styles.btnEndereco} mb-5`} >
+                          {loading ? (
+                            <Spinner animation="border">
+                              <span className="visually-hidden">Loading...</span>
+                            </Spinner>
+                          ) : (
+                            <span>Alterar Dados</span>
+                          )}
+                        </button>
+                      </div>  
                   </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <ModalCadastro />
-        </div>
-      </div>
+                </div>  
+              </Form> 
+            </div>   
+            )}                  
+      </Formik>
     )
   }
 
