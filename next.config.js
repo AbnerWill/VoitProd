0b63cb6d2cff22e19dc542cbbb1d881476af54ca
Abner Willclefy <abnerwill.monteiro@gmail.com>
2021-11-23 09:23:15 -0300

    :lock: Adicionando baseurl para o cors

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     return [
       {
         // matching all API routes
-        source: "/api/:path*",
+        source: "http://54.162.88.62/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
